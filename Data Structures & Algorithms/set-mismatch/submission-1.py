f16bf35e79a065900c99cd42ba9640ecf6c56bc6
jdeguzman11 <138526393+jdeguzman11@users.noreply.github.com>
2026-08-13 22:42:08 -0700

    Add: set-mismatch - submission-1

---
+++ Data Structures & Algorithms/set-mismatch/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        seen = set()
+        duplicate = 0
+        missing = 0
+
+        for num in nums:
+            if num in seen:
+                duplicate = num
+            else:
+                seen.add(num)
+        
+        for num in range(1, len(nums) + 1):
+            if num not in seen:
+                missing = num
+                break
+        
+        return [duplicate, missing]
